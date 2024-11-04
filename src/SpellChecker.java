d35d5340d3c16660f696f691561167463161e2b6
paulawang <paulatwang@gmail.com>
2024-11-04 15:03:45 -0500

    Added instructions to SpellChecker

--- src/SpellChecker.java
+++ src/SpellChecker.java
@@ -8,6 +8,44 @@ public class SpellChecker {
     public static void start() {
       // TODO: You can modify the body of this method,
       // or you can leave it blank. You must keep the signature, however.
+
+        /*
+         * Step1: prompt the user for filename for dictionary
+         * - if file doesn't exist, repeatedly prompt
+         * - if file does exist, print name
+         */
+
+
+
+        /*
+         *  Step 2: prompt the user for filename to be spellchecked
+         * -  if file doesn't exist, repeatedly prompt
+         * - if file does exist, print name of output file
+         */
+
+
+
+        /*
+        * Step 3: Do spell checking
+        * - iterate through each word in the spellchecked file
+        * - if the word is in the dictionary file, add to output file
+        * - if the word is not in dictionary file, prompt user to select 'r', 'a', or 't'
+        *   - if 'r':
+        *       - identify suggested words for replacement (call WordRecommender)
+        *       - prompt user to select replacement, repeatedly prompt if input invalid
+        *       - replace word in output file
+        *       - if no suggestions available, then prompt user to reselect 'a' or 't'
+        *   - if 'a':
+        *       - leave misspelled word unchanged
+        *       - add word to output file
+        *   - if 't':
+        *       - prompt user for new spelling
+        *       - replace word in output file
+        * */
+
+        /*
+        * Step 4:
+        * */
     }
   
     // You can of course write other methods as well.
